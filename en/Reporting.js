cf1dd6aba2deaf70257f33824dbccb1d5c4237e9
tanvidom <tanvidomadia15@gmail.com>
2017-06-28 15:46:46 +0530

    Adding code in reporting.js for gstudio implementation. To be altered by team in all three languages

--- en/Reporting.js
+++ en/Reporting.js
@@ -75,6 +75,8 @@ var JsonArray =
 }; 
 //pass the method to calculate score.
 //window.opener.saveDataOnExit(JsonArray);
+
+
 }
 
 function stageEnd( mmission,stagePlayed,replayCount,isPassed,caseDetails,scoreIN,stageStar){
@@ -102,4 +104,55 @@ var JsonArray =
 //window.opener.saveDataStages(JsonArray);
 //console.log(JsonArray);
 }
+//starting code for gstudio
+/*var somevariavb =0;
+function opneinnewindow(){
+somevariavb = window.open('/modules/policequadv2/index.html');
+}
+function saveDataOnExit(JsonArray)
+{
+gameReporter.submitData('/dev-utilities/tools-test/', JsonArray)
+console.log('hi');
+}
+function saveDataStages(JsonArray)
+{
+gameReporter.submitData('/dev-utilities/tools-test/', JsonArray)
+console.log('hi-2');
+}
+class GameReporter 
+{
+	constructor(data) {
+		this.session_id = this.getCookie('session_uuid')
+	}
+
+	submitData(url, data) {
+	
+
+
+           	var xhr = new XMLHttpRequest();
+		var data_string = {}
+		data_string['session_id'] = this.getCookie('session_uuid');
+		for (var key in data) {data_string[key] = data[key];};
+		data_string = JSON.stringify(data_string);
+
+		xhr.open('POST', url, false);
+		xhr.setRequestHeader("Content-Type","application/json");
+		xhr.send(data_string);
+		return xhr.response
+	}
+	getCookie(cname) {
+		var name = cname + "=";
+		var ca = document.cookie.split(';');
+		for(var i=0; i<ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0)==' ') c = c.substring(1);
+			if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+		}
+        console.log('no uuid found')
+	}
+}
+var gameReporter = new GameReporter();
+//returnGameReporter();
+*/
+
 
--- hi/Reporting.js
+++ hi/Reporting.js
@@ -102,21 +102,57 @@ var JsonArray =
 //window.opener.saveDataStages(JsonArray);
 //console.log(JsonArray);
 }
-var somevariavb =0;
+
+//starting code for gstudio
+/*var somevariavb =0;
 function opneinnewindow(){
-	console.log('no data');
 somevariavb = window.open('/modules/policequadv2/index.html');
 }
 function saveDataOnExit(JsonArray)
 {
-
-//gameReporter.submitData('/api/appdata/', JsonArray)
+gameReporter.submitData('/dev-utilities/tools-test/', JsonArray)
 console.log('hi');
 }
 function saveDataStages(JsonArray)
 {
-//gameReporter.submitData('/api/appdata/', JsonArray)
+gameReporter.submitData('/dev-utilities/tools-test/', JsonArray)
 console.log('hi-2');
 }
+class GameReporter 
+{
+	constructor(data) {
+		this.session_id = this.getCookie('session_uuid')
+	}
+
+	submitData(url, data) {
+	
+
+
+           	var xhr = new XMLHttpRequest();
+		var data_string = {}
+		data_string['session_id'] = this.getCookie('session_uuid');
+		for (var key in data) {data_string[key] = data[key];};
+		data_string = JSON.stringify(data_string);
+
+		xhr.open('POST', url, false);
+		xhr.setRequestHeader("Content-Type","application/json");
+		xhr.send(data_string);
+		return xhr.response
+	}
+	getCookie(cname) {
+		var name = cname + "=";
+		var ca = document.cookie.split(';');
+		for(var i=0; i<ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0)==' ') c = c.substring(1);
+			if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+		}
+        console.log('no uuid found')
+	}
+}
+var gameReporter = new GameReporter();
+//returnGameReporter();
+*/
+
 
 
